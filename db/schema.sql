7931cf75d883de1a6ddb35a6c808cae4a6048ab1
Yemny <y.feliz1296@gmail.com>
2023-10-05 18:40:31 -0400

    possible book api and schema file

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS books_db;
+CREATE DATABASE books_db;
+
+USE books_db;
+
+CREATE TABLE book_info (
+    id INT PRIMARY KEY NOT NULL,
+    title
+    author
+    isbn
+    publisheddate
+    genre
+    description
+
+)
+
