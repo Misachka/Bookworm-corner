8b4305e9e2fb9c88c7c54fb63a18aa40608ed450
Yemny <y.feliz1296@gmail.com>
2023-10-06 20:37:09 -0400

    sketch book table in db/schema.sql and started get request for book data and connection to local database

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,10 @@ CREATE DATABASE books_db;
 USE books_db;
 
 CREATE TABLE book_info (
-    id INT PRIMARY KEY NOT NULL,
-    title
-    author
-    isbn
-    publisheddate
-    genre
-    description
-
+   title VARCHAR(30) PRIMARY KEY NOT NULL,
+   author VARCHAR(30) NOT NULL,
+   description VARCHAR(30),
+   category VARCHAR(30) NOT NULL
+   
 )
 
--- public/js/bookapi.js
+++ public/js/bookapi.js
@@ -21,19 +21,29 @@ db.connect((err) => {
 
 const getBooks = async (book) => {
     const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${book}&key=AIzaSyAi3EIdAR7i4QzZGHPltWG5xfkBqiVo9vg`
       
     );
     const data = await response.json();
+
+    const title = data[title];
+    const author = data[authors];
+    const description = data[description];
+    const category = data[categories];
+    //const isbn = data
+    const cover = data[smallThumbnail];
+
     return data;
   };
 //set variables for title, author, description, genre, publisheddate, isbn
+
 
 db.connect(db, (err, client, done) => {
     done();
     if(err) {
         console.log(err);
     }
-    //client.query('INSERT INTO')
+    client.query('INSERT INTO book_info(title, author, description, category, isbn) VALUES()')
 
 })
 
