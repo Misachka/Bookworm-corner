5cb0cca9409b845b69321dbfffcecb3dfa293908
Yemny <y.feliz1296@gmail.com>
2023-10-10 23:52:17 -0400

    forEach loop for cards in genre.js

--- public/js/genre.js
+++ public/js/genre.js
@@ -6,24 +6,48 @@ $("#genre-list button").on("click", function(event) {
     .then(res => res.json())
     .then(data => {
         console.log(data)
+        data.forEach((data) => {
+            
+        const newDiv = $("<div>");
+        newDiv.addClass("card");
+        //newDiv.css("width", "18rem");
+        
+
+        newDiv.html(`
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+        `)
+
+        $("#results-list").append(newDiv)
+
+        })
 
         // create a for loop that will create a card
 
         const newDiv = $("<div>");
         newDiv.addClass("card");
-        newDiv.css("width", "18rem");
+        //newDiv.css("width", "18rem");
+        
 
         newDiv.html(`
-        <img class="card-img-top" src="https://placehold.co/600x400" alt="Card image cap">
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
         <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
+          <h5 class="card-title">title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
+      </div>
         `)
 
         $("#results-list").append(newDiv)
 
-        ///
+        
     })
 })
 
