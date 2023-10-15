1bfce3ef43139e9a8c6247ad303cb25fb04c1cf8
Misachka <136197530+Misachka@users.noreply.github.com>
2023-10-15 15:52:57 -0400

    added more genres

--- public/js/login.js
+++ public/js/login.js
@@ -1,55 +1,55 @@
 // Will connect to the homepage handlebars
 
 const loginPage= async (event) => {
-    event.preventDefault();
-  
-   
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password').value.trim();
-  
-    if (email && password) {
+  event.preventDefault();
 
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-  
-        window.location.replace('/home');
-      } else {
-        alert(response.statusText);
-      }
+ 
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
+
+  if (email && password) {
+
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+
+      window.location.replace('/home');
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  const newReader = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#username').value.trim();
-    const email = document.querySelector('#new-email').value.trim();
-    const password = document.querySelector('#new-pass').value.trim();
-  
-    if (name && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        window.location.replace('/home');
-      } else {
-        alert(response.statusText);
-      }
+  }
+};
+
+const newReader = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#username').value.trim();
+  const email = document.querySelector('#new-email').value.trim();
+  const password = document.querySelector('#new-pass').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      window.location.replace('/home');
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginPage);
-  
-  document
-    .querySelector('.newUser-form')
-    .addEventListener('submit', newReader);
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginPage);
+
+document
+  .querySelector('.newUser-form')
+  .addEventListener('submit', newReader);
