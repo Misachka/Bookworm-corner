f84a7932ee83e1d3e451bd0872d4ac7f8acb8ba9
Yemny <y.feliz1296@gmail.com>
2023-10-15 00:19:12 -0400

    added css to cards for books, took the books div out of the nav section in the home.handlebars

--- public/js/genre.js
+++ public/js/genre.js
@@ -2,13 +2,13 @@ $("#genre-list button").on("click", function(event) {
   const genre = event.target.textContent;
   console.log("getting books for " + genre);
 
-  fetch(`https://www.googleapis.com/books/v1/volumes?q=category:${genre}&key=AIzaSyAi3EIdAR7i4QzZGHPltWG5xfkBqiVo9vg`)
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&key=AIzaSyAi3EIdAR7i4QzZGHPltWG5xfkBqiVo9vg`)
     .then(res => res.json())
     .then(data => {
       //console.log(data);
       const books = data.items;
       console.log(books);
-      //const booksContainer = document.querySelector('#results-list');
+      const booksContainer = document.querySelector('#results-list');
       //function renderBooks(books){
 
       
@@ -16,18 +16,22 @@ $("#genre-list button").on("click", function(event) {
 
         const newDiv = $("<div>");
         newDiv.addClass("card");
-        newDiv.css("width", "18rem");
+        //newDiv.css("width", "30rem");
 
         newDiv.html(`
-          <div class="card">
+          <div class="m-2 card">
             <img class="card-img-top" src="${book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail : 'https://via.placeholder.com/128x192'}" alt="Book Cover">
             <div class="card-body">
               <h5 class="card-title">${book.volumeInfo.title}</h5>
-              <p class="card-text">${book.volumeInfo.description || 'No description available'}</p>
+              
               <button class="btn btn-primary" data-title="${book.volumeInfo.title}">Add to favorites</button>
             </div>
           </div>
         `);
+
+
+
+    
 
         $("#results-list").append(newDiv);
       })
