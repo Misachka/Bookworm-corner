46e6d7c11aaeb8784bd13c9d7b6df00d50672e28
K1tanaK0mbat <kkeayouth@gmail.com>
2023-10-16 12:55:16 -0500

    added seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,2 +1,25 @@
 const sequelize = require('../config/connection');
 const { User, Book, Favorites } = require('../models');
+
+const userData = require('./userData.json');
+const favData = require('./favData.json');
+const bookData = require('./bookData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const fav of favData) {
+        await Favorites.create({
+            ...fav,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
+        process.exit(0);
+    };
+
+    seedDatabase();
