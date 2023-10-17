ad19d4679a5e39d1d7dba50cf9917d26170d5daa
Yemny <y.feliz1296@gmail.com>
2023-10-16 23:58:01 -0400

    trying to set key to environment and fix search bar

--- public/js/genre.js
+++ public/js/genre.js
@@ -1,8 +1,9 @@
 $("#genre-list button").on("click", function(event) {
+
   const genre = event.target.textContent;
   console.log("getting books for " + genre);
 
-  fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&key=AIzaSyAi3EIdAR7i4QzZGHPltWG5xfkBqiVo9vg`)
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&key=${process.env.GOOGLE_KEY}`)
     .then(res => res.json())
     .then(data => {
       //console.log(data);
@@ -44,6 +45,48 @@ $("#genre-list button").on("click", function(event) {
       
        });
   });
+
+  //search-bar
+  $("#search-btn").on("click", function(){
+    let input = document.querySelector(".search-bar").value;
+    //input = input.toLowerCase();
+    //console.log("getting books for " + genre);
+
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=${input}&key=${process.env.GOOGLE_KEY}`)
+    .then(res => res.json())
+    .then(data => {
+      //console.log(data);
+      const books = data.items;
+      console.log(books);
+      const booksContainer = document.querySelector('#results-list');
+
+      $('#results-list').empty();
+
+
+      books.forEach((book) => {
+
+        const newDiv = $("<div>");
+        newDiv.addClass("card");
+        //newDiv.css("width", "30rem");
+
+        newDiv.html(`
+          <div class="m-2 card">
+            <img class="card-img-top" src="${book.volumeInfo.imageLinks.s ? book.volumeInfo.imageLinks.smallThumbnail : 'https://via.placeholder.com/128x192'}" alt="Book Cover">
+            <div class="card-body">
+              <h5 class="card-title">${book.volumeInfo.title}</h5>
+              
+              <button class="btn btn-primary" data-title="${book.volumeInfo.title}">Add to favorites</button>
+            </div>
+          </div>
+        `);
+
+        $("#results-list").append(newDiv);
+      })
+    //newDiv.css("width", "18rem");
+
+
+       });
+  })
   
   
 $("#results-list").on("click", ".add-to-favorites", function(event) {
